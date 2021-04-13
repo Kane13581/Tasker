286efff930a546957bd91d626c3af15f799779fa
Istrate-Andrei <56413983+Kane13581@users.noreply.github.com>
2021-04-14 00:30:41 +0300

    implemented server

--- src/components/AddTask.vue
+++ src/components/AddTask.vue
@@ -1,10 +1,10 @@
 <template>
-<form @submit="onSubmit" class="add-form"> 
-<div class="form-control">
-<label>Task</label>
-<input type="text" v-model="text" name="text" placeholder="Add Task"/>
-</div>
-<div class="form-control">
+	<form @submit="onSubmit" class="add-form"> 
+	<div class="form-control">
+		<label>Task</label>
+		<input type="text" v-model="text" name="text" placeholder="Add Task"/>
+	</div>
+	<div class="form-control">
       <label>Day & Time</label> 
       <input type="text" v-model="day" name="day" placeholder="Add Day & Time" />
     </div>
@@ -20,6 +20,37 @@
 <script>
 export default {
 	name: 'AddTask',
+	data() {
+		return {
+			text: '',
+			day: '',
+			reminder: false
+		}
+	},
+	methods: {
+		onSubmit(e) {
+			e.preventDefault()
+
+			if(!this.text) {
+				alert("Please add a task")
+				return
+			}
+
+			const newTask = {
+				//id: Math.floor(Math.random() * 100000),
+				text: this.text,
+				day: this.day,
+				reminder: this.reminder
+			}
+
+			this.$emit('add-task', newTask);
+
+			this.text = ''
+			this.day = ''
+			this.reminder = false
+
+		}
+	}
 }
 </script>
 
